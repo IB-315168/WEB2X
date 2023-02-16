6796a06c5023a21323d3a76edbfc7d73cb0afbad
IB-315168 <315168@via.dk>
2023-02-16 12:28:41 +0100

    Exercise 2.1

--- Session2/ex1.js
+++ Session2/ex1.js
@@ -1,5 +1,5 @@
 function squareList(array) {
-    return array.filter(i => typeof i == Number).map(x => x * x)
+    return array.filter(i => Number.isInteger(i) && i > 0).map(x => x * x)
 }
 
-console.log(squareList([1,2,3]))
+console.log(squareList([-3, 4.8, 5, 3, -3.2]))
